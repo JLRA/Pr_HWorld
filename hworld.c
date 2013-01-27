169ed648c64448c79d5996ef9fc8458d67233573
lupen <lupen@lupen-Aspire-5750G.(none)>
2013-01-27 13:46:55 +0100

    Version 004

--- hworld.c
+++ hworld.c
@@ -2,7 +2,7 @@
 
 void main(void){
   printf("\n\n====================================\n");
-  printf("Esto es otra prueba de Hola mundo\n");
-  printf("Esto es una prueba de Hola mundo(2)!");
+  printf("Esto es otra prueba de Hola mundo - 4\n");
+  printf("Esto es una prueba de Hola mundo(4!)!");
   printf("\n====================================\n\n");
 }
