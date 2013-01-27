958aa8782ca19e739dc7ae47b1f1b816ba16232c
JLRA <codestore82@hotmail.com>
2013-01-27 13:57:15 +0100

    Version 007

--- hworld.c
+++ hworld.c
@@ -2,7 +2,8 @@
 
 void main(void){
   printf("\n\n====================================\n");
-  printf("Esto es otra prueba de Hola mundo - 6!!\n");
-  printf("Esto es una prueba de Hola mundo(6!!)!");
+  printf("Esto es otra prueba de Hola mundo - 7!!\n");
+  printf("Estamso en la verison 7 ya knijo!\n");
+  printf("Esto es una prueba de Hola mundo(7!!)!");
   printf("\n====================================\n\n");
 }
