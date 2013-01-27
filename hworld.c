fecf3ba1ad3efcff590a60708d946f0b448a7153
lupen <lupen@lupen-Aspire-5750G.(none)>
2013-01-27 13:24:26 +0100

    Version 002

--- hworld.c
+++ hworld.c
@@ -1,5 +1,7 @@
 #include "stdio.h"
 
 void main(void){
+  printf("\n\n====================================\n");
   printf("Esto es una prueba de Hola mundo!");
+  printf("\n====================================\n\n");
 }
